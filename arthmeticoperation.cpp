219ab6c66d1c56af5d4cf946a600e839f63fa3cb
jyoshnanagareddy <96761088+jyoshnanagareddy@users.noreply.github.com>
2021-12-28 11:23:45 +0530

    Add files via upload
    arithmetic operation in c++ language

---
+++ arthmeticoperation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int A,B,C,D,E,F;
+    cin>>A>>B;
+	C=A+B;
+	D=A-B;
+	E=A*B;
+	F=A/B;
+	cout <<"sum is:"<<C<<endl;
+	cout <<"difference is:"<<D<<endl;
+	cout <<"multiplication is:"<<E<<endl;
+	cout <<"division is:"<<F<<endl;
+	return 0;
+}
